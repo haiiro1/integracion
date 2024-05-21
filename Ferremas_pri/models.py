5e5a6b61aa0a7e050818bba54203e8b1580b1bd8
InaCiero <ig.ciero@duocuc.cl>
2024-05-21 15:29:58 -0400

    0.0.5
    Definición de modelos para productos y carrito

--- Ferremas_pri/models.py
+++ Ferremas_pri/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+#                           Productos~
+
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    detalle = models.TextField()
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+#                           Carrito~
+
+class Carrito(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto, through='ItemCarrito')
+
+class ItemCarrito(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
