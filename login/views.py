32e7602985d7286d1699a5b798d3d03dc1a24c8a
InaDayo~ <ig.ciero@duocuc.cl>
2024-05-23 14:17:35 -0400

    Update views.py

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm,StaffUserCreationForm
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import CustomUser
 
