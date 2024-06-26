c7ea436c949e3548f18bdf4cd5ce0d86bc09c438
InaCiero <ig.ciero@duocuc.cl>
2024-06-26 19:49:34 -0400

    0.2
    Test de cambio de divisa integrado.

--- Ferremas_pri/tests.py
+++ Ferremas_pri/tests.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from .models import Producto
+import unittest
+from unittest.mock import patch, MagicMock
+from .api import Mindicador
 
 class ProductoTests(TestCase):
 
@@ -24,3 +27,37 @@ class ProductoTests(TestCase):
         response = self.client.get(url_detalle)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['nombre'], 'Producto de Prueba')
+
+class TestMindicador(unittest.TestCase):
+
+    @patch('requests.get')
+    def test_get_dolar_price_success(self, mock_get):
+        # Configurar el mock de requests.get para simular una respuesta exitosa
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.text = '{"serie":[{"valor":800}]}'
+        mock_get.return_value = mock_response
+
+        # Instanciar Mindicador y llamar get_dolar_price
+        mindicador = Mindicador('dolar', 2024)
+        dolar_price = mindicador.get_dolar_price()
+
+        # Verificar que el precio del dólar obtenido sea correcto
+        self.assertEqual(dolar_price, 800)
+
+    @patch('requests.get')
+    def test_get_dolar_price_failure(self, mock_get):
+        # Configurar el mock de requests.get para simular una respuesta fallida
+        mock_response = MagicMock()
+        mock_response.status_code = 404
+        mock_get.return_value = mock_response
+
+        # Instanciar Mindicador y llamar get_dolar_price
+        mindicador = Mindicador('dolar', 2024)
+        dolar_price = mindicador.get_dolar_price()
+
+        # Verificar que el precio del dólar sea None cuando la solicitud falla
+        self.assertIsNone(dolar_price)
+
+if __name__ == '__main__':
+    unittest.main()
